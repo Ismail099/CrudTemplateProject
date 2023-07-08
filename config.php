2c5b5d2752743c6a2dfc5144519fb63da3115cb0
3301-byte <hedaribo09@gmail.com>
2023-07-08 22:53:37 +0000

    Upload New File

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Database credentials.
+ */
+
+define('DB_SERVER', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '!MySQLRoot1978.');
+define('DB_NAME', 'crud_app');
